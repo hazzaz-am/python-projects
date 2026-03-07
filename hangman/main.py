a1ce999c22eb87fc708648991b833a3f0b156a14
hazzaz-am <hazzazabdulmannan@gmail.com>
2026-03-07 14:28:20 +0600

    implement main game logic and display functions for hangman

--- hangman/main.py
+++ hangman/main.py
@@ -236,6 +236,65 @@ hangman_art = {
     6: (" o ", "/|\\", "/ \\"),
 }
 
-for line in hangman_art[0]:
-  print(line)
-  
+
+def display_man(wrong_guesses: int):
+    print("**********")
+    for line in hangman_art[wrong_guesses]:
+        print(line)
+    print("**********")
+
+
+def display_hint(hint: list[str]):
+
+    print(" ".join(hint))
+
+
+def display_answer(answer: str):
+
+    print(" ".join(answer))
+
+
+def main():
+    answer: str = random.choice(words)
+    hint = ["_"] * len(answer)
+    wrong_guesses = 0
+    guessed_letters: set[str] = set()
+    is_running = True
+
+    while is_running:
+        display_man(wrong_guesses)
+        display_hint(hint)
+        guess = input("Enter a letter: ").lower()
+
+        if len(guess) != 1 or not guess.isalpha():
+            print("Invalid input")
+            continue
+
+        if guess in guessed_letters:
+            print(f"{guess} is already guessed")
+            continue
+        
+        guessed_letters.add(guess)
+
+        if guess in answer:
+            for i in range(len(answer)):
+                if answer[i] == guess:
+                    hint[i] = guess
+        else:
+            wrong_guesses += 1
+        
+        if "_" not in hint:
+            display_man(wrong_guesses)
+            display_answer(answer)
+            print("YOU WIN!")
+            is_running= False
+        elif wrong_guesses >= len(hangman_art) - 1:
+            display_man(wrong_guesses)
+            display_answer(answer)
+            print("YOU LOSE")
+            is_running = False
+        
+
+
+if __name__ == "__main__":
+    main()
